3e8f3acc12a600c53016288e85c25d30dc9db2de
klemenp950 <klemen.parkelj@gmail.com>
2024-04-22 22:26:22 +0200

    Naloga 2 14/36

--- Naloga 2/server.py
+++ Naloga 2/server.py
@@ -1,6 +1,7 @@
 """An example of a simple HTTP server."""
 import json
 import mimetypes
+import os
 import pickle
 import socket
 from os import listdir
@@ -14,7 +15,15 @@ PICKLE_DB = "db.pkl"
 WWW_DATA = "www-data"
 
 # Header template for a successful HTTP request
-HEADER_RESPONSE_200 = """HTTP/1.1 200 OK\r
+RESPONSE_200 = """HTTP/1.1 200 OK\r
+content-type: %s\r
+content-length: %d\r
+connection: Close\r
+\r
+"""
+
+RESPONSE_301 = """HTTP/1.1 301 Moved Permanently\r
+location: %s\r
 content-type: %s\r
 content-length: %d\r
 connection: Close\r
@@ -30,7 +39,15 @@ TABLE_ROW = """
 </tr>
 """
 
-# Template for a 404 (Not found) error
+RESPONSE_400 = """HTTP/1.1 400 Bad Request\r
+Content-Type: text/html; charset=iso-8859-1\r
+Connection: Closed\r
+\r
+<!doctype html>
+<h1>400 Bad Request</h1>
+<p>Bad Request</p>
+"""
+
 RESPONSE_404 = """HTTP/1.1 404 Not found\r
 content-type: text/html\r
 connection: Close\r
@@ -38,6 +55,15 @@ connection: Close\r
 <!doctype html>
 <h1>404 Page not found</h1>
 <p>Page cannot be found.</p>
+"""
+
+RESPONSE_405 = """HTTP/1.1 405 Method not allowed\r
+content-type: text/html\r
+connection: Close\r
+\r
+<!doctype html>
+<h1>405 Method not allowed</h1>
+<p>Method not allowed.</p>
 """
 
 DIRECTORY_LISTING = """<!DOCTYPE html>
@@ -117,6 +143,18 @@ def read_from_db(criteria=None):
         return []
 
 
+def preisci_mapo(ime, pot):
+    for el in os.listdir(pot):
+        polno_ime = os.path.join(pot, el)
+        if os.path.isdir(polno_ime):
+            pot_datoteke = preisci_mapo(ime, polno_ime)
+            if pot_datoteke:
+                return pot_datoteke
+        elif el == ime:
+            return polno_ime
+    return None
+
+
 def process_request(connection, address):
     """Process an incoming socket request.
 
@@ -134,6 +172,47 @@ def process_request(connection, address):
 
     # Write the response back to the socket
 
+    client = connection.makefile("wrb")
+    line = client.readline().decode("utf-8").strip()
+
+    try:
+        metoda, uri, version = line.split()
+        ime = uri.split("/")[-1]
+        if metoda != "GET" and metoda != "POST":
+            client.write(RESPONSE_405.encode("utf8"))
+        elif version != "HTTP/1.1" or not os.path.isfile("./www-data" + uri):
+            client.write(RESPONSE_400.encode("utf8"))
+        elif not os.path.exists("./www-data" + uri):
+            pot = ".\\" + preisci_mapo(ime, "www-data")
+            if pot:
+                with open(pot, "rb") as h:
+                    body = h.read()
+                mime_type, _ = mimetypes.guess_type(pot)
+                pot = pot.replace("\\", "/")
+                header = RESPONSE_301 % (
+                    pot[10:],
+                    mime_type,
+                    len(body)
+                )
+                client.write(header.encode("utf8"))
+                client.close()
+            else:
+                client.write(RESPONSE_404.encode("utf"))
+        elif os.path.exists("./www-data" + uri) and os.path.isfile("./www-data" + uri):
+            with open(("www-data" + uri), "rb") as h:
+                body = h.read()
+            mime_type, _ = mimetypes.guess_type(uri)
+            header = RESPONSE_200 % (
+                mime_type,
+                len(body)
+            )
+            client.write(header.encode("utf8"))
+            client.write(body)
+    except Exception as e:
+        print(e)
+        client.write(RESPONSE_400.encode("utf8"))
+    client.close()
+
 
 def main(port):
     """Starts the server and waits for connections."""
@@ -144,7 +223,6 @@ def main(port):
     server.listen(1)
 
     print("Listening on %d" % port)
-
     while True:
         connection, address = server.accept()
         print("[%s:%d] CONNECTED" % address)
@@ -155,3 +233,4 @@ def main(port):
 
 if __name__ == "__main__":
     main(8080)
+
